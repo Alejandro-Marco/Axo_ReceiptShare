c1dd438dd47f73c06ce4ffa553630ca9e51c78ac
Alejandro Alejandre <alejandroalejandre15@gmail.com>
2021-06-28 00:32:11 +0800

    Changes in Constants

--- app/src/main/java/com/axolotl/receiptmanager/utility/Constants.kt
+++ app/src/main/java/com/axolotl/receiptmanager/utility/Constants.kt
@@ -9,3 +9,12 @@ const val MAIN_ACTIVITY = "my:MainActivity"
 const val PATH_RECEIPT = "Receipts"
 
 const val HELPER = "Helper"
+const val UPLOAD_PROMPT_WAIT_TIME: Long = 5000
+val UPLOAD_PROMPTS = arrayListOf<String>(
+    "Still uploading",
+    "It's taking a while",
+    "Uploading",
+    "Anytime now"
+)
+
+
